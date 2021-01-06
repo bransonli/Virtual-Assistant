150b8eeba5336575753e910772d379725171c294
bransonli <70127104+bransonli@users.noreply.github.com>
2021-01-06 21:25:25 +0800

    Add files via upload

--- virtual assistant.py
+++ virtual assistant.py
@@ -27,11 +27,40 @@ print(r.recognize_ibm(audio)) #convert the audio file into text
 
 ### input to audio file conversion
 #guide for audio receving https://stackoverflow.com/questions/35344649/reading-input-sound-signal-using-python
+#documentation on https://people.csail.mit.edu/hubert/pyaudio/docs/
 
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, #16 bit binary string 
-                channels=2, #number of stereo outputs
-                rate= 44100,
-                input=True
-                frames_per_buffer==1024)
 
+def record_wav():
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16 #16 bit binary string 
+    CHANNELS = 2 #number of stereo outputs
+    RATE = 44100
+    RECORD_SECONDS = 5
+    WAVE_OUTPUT_FILENAME = "output.wav"
+
+    p = pyaudio.PyAudio() #initializes pyaudio 
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK) #starts recording 
+
+    frames = []
+
+###### still to be changed 
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    stream.stop_stream() #stop 
+    stream.close()
+    p.terminate()
+######
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb') #creates and store into a wave file
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
